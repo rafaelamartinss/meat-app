50b96730b184da35d16e1eb1c9fa04d1f6498291
rafaelamartinss <rafaelalealmartinss@gmail.com>
2019-10-30 15:52:36 -0300

    componente de restaurante - basico

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restautant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
